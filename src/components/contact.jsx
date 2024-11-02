c8fae6003042465b844bc682ae6a65099ed12b97
eduardocarlostoledo <eduardocarlostoledo@gmail.com>
2024-11-02 20:48:30 -0300

    emailjs

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import emailjs from "emailjs-com";
 import React from "react";
 
 const initialState = {
@@ -7,6 +6,7 @@ const initialState = {
   email: "",
   message: "",
 };
+
 export const Contact = (props) => {
   const [{ name, email, message }, setState] = useState(initialState);
 
@@ -14,27 +14,43 @@ export const Contact = (props) => {
     const { name, value } = e.target;
     setState((prevState) => ({ ...prevState, [name]: value }));
   };
+
   const clearState = () => setState({ ...initialState });
-  
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(name, email, message);
-    
-    {/* replace below with your own Service ID, Template ID and Public Key from your EmailJS account */ }
-    
-    emailjs
-      .sendForm("REACT_APP_YOUR_SERVICE_ID", "REACT_APP_YOUR_TEMPLATE_ID", e.target, "REACT_APP_YOUR_PUBLIC_KEY")
-      .then(
-        (result) => {
-          console.log(result.text);
+
+    const data = {
+      service_id: process.env.REACT_APP_SERVICE_ID,
+      template_id: process.env.REACT_APP_TEMPLATE_ID,
+      user_id: process.env.REACT_APP_PUBLIC_KEY,
+      template_params: {
+        name: name,
+        email: email,
+        message: message,
+      },
+    };
+
+    fetch("https://api.emailjs.com/api/v1.0/email/send", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log("Email sent successfully");
           clearState();
-        },
-        (error) => {
-          console.log(error.text);
+        } else {
+          return response.text().then((text) => {
+            throw new Error(text);
+          });
         }
-      );
+      })
+      .catch((error) => console.error("Failed to send email:", error));
   };
+
   return (
     <div>
       <div id="contact">
@@ -44,7 +60,8 @@ export const Contact = (props) => {
               <div className="section-title">
                 <h2>Mantengamos el contacto</h2>
                 <p>
-                  Dejanos un mensaje o consulta y estaremos en comunicación a la brevedad posible.
+                  Dejanos un mensaje o consulta y estaremos en comunicación a la
+                  brevedad posible.
                 </p>
               </div>
               <form name="sentMessage" validate onSubmit={handleSubmit}>
@@ -97,68 +114,186 @@ export const Contact = (props) => {
               </form>
             </div>
           </div>
-          <div className="col-md-3 col-md-offset-1 contact-info">
-            <div className="contact-item">
-              <h3>Información de contacto</h3>
-              <p>
-                <span>
-                  <i className="fa fa-map-marker"></i> Address
-                </span>
-                {props.data ? props.data.address : "loading"}
-              </p>
-            </div>
-            <div className="contact-item">
-              <p>
-                <span>
-                  <i className="fa fa-phone"></i> Phone
-                </span>{" "}
-                {props.data ? props.data.phone : "loading"}
-              </p>
-            </div>
-            <div className="contact-item">
-              <p>
-                <span>
-                  <i className="fa fa-envelope-o"></i> Email
-                </span>{" "}
-                {props.data ? props.data.email : "loading"}
-              </p>
-            </div>
-          </div>
-          <div className="col-md-12">
-            <div className="row">
-              <div className="social">
-                <ul>
-                  <li>
-                    <a href={props.data ? props.data.facebook : "/"}>
-                      <i className="fa fa-facebook"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href={props.data ? props.data.whatsapp : "/"}>
-                      <i className="fa fa-whatsapp"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href={props.data ? props.data.youtube : "/"}>
-                      <i className="fa fa-youtube"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div id="footer">
-        <div className="container text-center">
-          <p>
-            &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
-            <a href="http://www.templatewire.com" rel="nofollow">
-              TemplateWire
-            </a>
-          </p>
+          {/* Información de contacto y redes sociales */}
         </div>
       </div>
     </div>
   );
 };
+
+// import { useState } from "react";
+// import emailjs from "emailjs-com";
+// import React from "react";
+
+// const initialState = {
+//   name: "",
+//   email: "",
+//   message: "",
+// };
+
+// export const Contact = (props) => {
+//   const [{ name, email, message }, setState] = useState(initialState);
+
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setState((prevState) => ({ ...prevState, [name]: value }));
+//   };
+
+//   const clearState = () => setState({ ...initialState });
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+
+//     // Asegúrate de reemplazar con tus propios IDs y claves
+//     console.log( process.env.REACT_APP_SERVICE_ID,
+//       process.env.REACT_APP_TEMPLATE_ID,
+//       e.target,
+//       process.env.REACT_APP_PUBLIC_KEY)
+//     emailjs
+//       .sendForm(
+//         process.env.REACT_APP_SERVICE_ID,
+//         process.env.REACT_APP_TEMPLATE_ID,
+//         e.target,
+//         process.env.REACT_APP_PUBLIC_KEY
+//       )
+//       .then(
+//         (result) => {
+//           console.log(result, result.text);
+//           clearState();
+//         },
+//         (error) => {
+//           console.log(error.text);
+//         }
+//       );
+//   };
+
+//   return (
+//     <div>
+//       <div id="contact">
+//         <div className="container">
+//           <div className="col-md-8">
+//             <div className="row">
+//               <div className="section-title">
+//                 <h2>Mantengamos el contacto</h2>
+//                 <p>
+//                   Dejanos un mensaje o consulta y estaremos en comunicación a la brevedad posible.
+//                 </p>
+//               </div>
+//               <form name="sentMessage" validate onSubmit={handleSubmit}>
+//                 <div className="row">
+//                   <div className="col-md-6">
+//                     <div className="form-group">
+//                       <input
+//                         type="text"
+//                         id="name"
+//                         name="name"
+//                         className="form-control"
+//                         placeholder="Nombre"
+//                         required
+//                         onChange={handleChange}
+//                         value={name}
+//                       />
+//                       <p className="help-block text-danger"></p>
+//                     </div>
+//                   </div>
+//                   <div className="col-md-6">
+//                     <div className="form-group">
+//                       <input
+//                         type="email"
+//                         id="email"
+//                         name="email"
+//                         className="form-control"
+//                         placeholder="Correo Electrónico"
+//                         required
+//                         onChange={handleChange}
+//                         value={email}
+//                       />
+//                       <p className="help-block text-danger"></p>
+//                     </div>
+//                   </div>
+//                 </div>
+//                 <div className="form-group">
+//                   <textarea
+//                     name="message"
+//                     id="message"
+//                     className="form-control"
+//                     rows="4"
+//                     placeholder="Mensaje"
+//                     required
+//                     onChange={handleChange}
+//                     value={message}
+//                   ></textarea>
+//                   <p className="help-block text-danger"></p>
+//                 </div>
+//                 <div id="success"></div>
+//                 <button type="submit" className="btn btn-custom btn-lg">
+//                   Enviar Mensaje
+//                 </button>
+//               </form>
+//             </div>
+//           </div>
+//           <div className="col-md-3 col-md-offset-1 contact-info">
+//             <div className="contact-item">
+//               <h3>Información de contacto</h3>
+//               <p>
+//                 <span>
+//                   <i className="fa fa-map-marker"></i> Dirección
+//                 </span>
+//                 {props.data ? props.data.address : "Cargando..."}
+//               </p>
+//             </div>
+//             <div className="contact-item">
+//               <p>
+//                 <span>
+//                   <i className="fa fa-phone"></i> Teléfono
+//                 </span>{" "}
+//                 {props.data ? props.data.phone : "Cargando..."}
+//               </p>
+//             </div>
+//             <div className="contact-item">
+//               <p>
+//                 <span>
+//                   <i className="fa fa-envelope-o"></i> Email
+//                 </span>{" "}
+//                 {props.data ? props.data.email : "Cargando..."}
+//               </p>
+//             </div>
+//           </div>
+//           <div className="col-md-12">
+//             <div className="row">
+//               <div className="social">
+//                 <ul>
+//                   <li>
+//                     <a href={props.data ? props.data.facebook : "/"}>
+//                       <i className="fa fa-facebook"></i>
+//                     </a>
+//                   </li>
+//                   <li>
+//                     <a href={props.data ? props.data.whatsapp : "/"}>
+//                       <i className="fa fa-whatsapp"></i>
+//                     </a>
+//                   </li>
+//                   <li>
+//                     <a href={props.data ? props.data.youtube : "/"}>
+//                       <i className="fa fa-youtube"></i>
+//                     </a>
+//                   </li>
+//                 </ul>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//       <div id="footer">
+//         <div className="container text-center">
+//           <p>
+//             &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
+//             <a href="http://www.templatewire.com" rel="nofollow">
+//               TemplateWire
+//             </a>
+//           </p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
