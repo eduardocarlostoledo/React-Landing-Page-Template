69f0983d6c2fa2ee0668a07b793e5f8de157c182
eduardocarlostoledo <eduardocarlostoledo@gmail.com>
2025-04-05 19:05:46 -0300

    insert logo in navbar

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -39,25 +39,29 @@ export const Navigation = (props) => {
           
         </div>
 
+        <a className="navbar-brand page-scroll" href="#page-top">
+            <img
+              src={logo}
+              alt="Toledo Consultora IT"
+              style={{ height: "80px", width: "auto" }} // ajustá el tamaño si es necesario
+            />
+          </a>
+
         <div
           className={`collapse navbar-collapse ${menuOpen ? "in" : ""}`}
           id="bs-example-navbar-collapse-1"
         >
+          
+
           <ul className="nav navbar-nav navbar-right">
-          <a className="navbar-brand page-scroll" href="#page-top">
-            <img
-              src={logo}
-              alt="Toledo Consultora IT"
-              style={{ height: "100px", width: "auto" }} // ajustá el tamaño si es necesario
-            />
-          </a>
+          
             <li><a href="#asesoramiento" className="page-scroll" onClick={handleLinkClick}>Que hacemos?</a></li>
             <li><a href="#acerca" className="page-scroll" onClick={handleLinkClick}>Acerca De</a></li>
             <li><a href="#partners" className="page-scroll" onClick={handleLinkClick}>Confían en Nosotros</a></li>
             <li><a href="#services" className="page-scroll" onClick={handleLinkClick}>Soluciones</a></li>
             <li><a href="#portfolio" className="page-scroll" onClick={handleLinkClick}>Portfolio</a></li>
             <li><a href="#testimonials" className="page-scroll" onClick={handleLinkClick}>Testimonios</a></li>
-            <li><a href="#team" className="page-scroll" onClick={handleLinkClick}>Equipo</a></li>
+            {/* <li><a href="#team" className="page-scroll" onClick={handleLinkClick}>Equipo</a></li> */}
             <li><a href="#pricing" className="page-scroll" onClick={handleLinkClick}>Pricing</a></li>
             <li><a href="#contact" className="page-scroll" onClick={handleLinkClick}>Contacto</a></li>
           </ul>
