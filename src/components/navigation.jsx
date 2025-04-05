48b1704123ab73dd5c30bf1f71454c04b9651c62
eduardocarlostoledo <eduardocarlostoledo@gmail.com>
2025-04-05 18:59:56 -0300

    insert logo in navbar

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import logo from "../components/images/logo.png"; // ajustá esta ruta según donde lo guardes
 
 export const Navigation = (props) => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -7,7 +8,6 @@ export const Navigation = (props) => {
     setMenuOpen(false);
   };
 
-  // Efecto para sincronizar el estado con las clases de Bootstrap
   useEffect(() => {
     const navbarCollapse = document.getElementById("bs-example-navbar-collapse-1");
     if (navbarCollapse) {
@@ -35,9 +35,8 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
           </button>
-          <a className="navbar-brand page-scroll" href="#page-top">
-            TOLEDO CONSULTORA IT
-          </a>
+
+          
         </div>
 
         <div
@@ -45,51 +44,22 @@ export const Navigation = (props) => {
           id="bs-example-navbar-collapse-1"
         >
           <ul className="nav navbar-nav navbar-right">
-            <li>
-              <a href="#asesoramiento" className="page-scroll" onClick={handleLinkClick}>
-                Que hacemos?
-              </a>
-            </li>
-            <li>
-              <a href="#acerca" className="page-scroll" onClick={handleLinkClick}>
-                Acerca De
-              </a>
-            </li>
-            <li>
-              <a href="#partners" className="page-scroll" onClick={handleLinkClick}>
-                Confían en Nosotros
-              </a>
-            </li>
-            <li>
-              <a href="#services" className="page-scroll" onClick={handleLinkClick}>
-                Soluciones
-              </a>
-            </li>
-            <li>
-              <a href="#portfolio" className="page-scroll" onClick={handleLinkClick}>
-                Portfolio
-              </a>
-            </li>
-            <li>
-              <a href="#testimonials" className="page-scroll" onClick={handleLinkClick}>
-                Testimonios
-              </a>
-            </li>
-            <li>
-              <a href="#team" className="page-scroll" onClick={handleLinkClick}>
-                Equipo
-              </a>
-            </li>
-            <li>
-              <a href="#pricing" className="page-scroll" onClick={handleLinkClick}>
-                Pricing
-              </a>
-            </li>
-            <li>
-              <a href="#contact" className="page-scroll" onClick={handleLinkClick}>
-                Contacto
-              </a>
-            </li>
+          <a className="navbar-brand page-scroll" href="#page-top">
+            <img
+              src={logo}
+              alt="Toledo Consultora IT"
+              style={{ height: "100px", width: "auto" }} // ajustá el tamaño si es necesario
+            />
+          </a>
+            <li><a href="#asesoramiento" className="page-scroll" onClick={handleLinkClick}>Que hacemos?</a></li>
+            <li><a href="#acerca" className="page-scroll" onClick={handleLinkClick}>Acerca De</a></li>
+            <li><a href="#partners" className="page-scroll" onClick={handleLinkClick}>Confían en Nosotros</a></li>
+            <li><a href="#services" className="page-scroll" onClick={handleLinkClick}>Soluciones</a></li>
+            <li><a href="#portfolio" className="page-scroll" onClick={handleLinkClick}>Portfolio</a></li>
+            <li><a href="#testimonials" className="page-scroll" onClick={handleLinkClick}>Testimonios</a></li>
+            <li><a href="#team" className="page-scroll" onClick={handleLinkClick}>Equipo</a></li>
+            <li><a href="#pricing" className="page-scroll" onClick={handleLinkClick}>Pricing</a></li>
+            <li><a href="#contact" className="page-scroll" onClick={handleLinkClick}>Contacto</a></li>
           </ul>
         </div>
       </div>
