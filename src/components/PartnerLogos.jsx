a691e5379f2e36810088437f24b7409d7a5180a8
eduardocarlostoledo <eduardocarlostoledo@gmail.com>
2025-07-10 09:05:11 -0300

    agregamos partners

--- src/components/PartnerLogos.jsx
+++ src/components/PartnerLogos.jsx
@@ -11,33 +11,20 @@ import logoappel from "../logoappel.jpg"; // Logo de la empresa
 import logoglobal from "../logoglobal.jpg"; // Logo de la empresa
 import logomodatotal from "../logomodatotal.png"; // Logo de la empresa
 import logolua from "../logolua.png"
+import logojuly from "../huallpa-logo.png"; // Logo de la empresa
 // JSON con los datos de los logos
 const partnerLogos = [
-  {
-    name: "Store Computer",
-    logo: "https://static.vecteezy.com/system/resources/thumbnails/009/096/946/small/monitor-screen-computer-with-colorful-rainbow-bubble-illustration-logo-design-vector.jpg", // Reemplazar con la URL real
-    url: "https://storecomputer.netlify.app/",
-  },
-  {
-    name: "El Gato Negro Premium - Eliquids",
-    logo: logo1,
-    url: "https://elgatonegropremium.netlify.app/",
+   {
+    name: "Yerba Mate Alborada",
+    logo: logoalborada,
+    url: "https://yerbamatealborada.com/",
   },
   {
     name: "Calyaan SaaS Colombia",
     logo: logoCalyaan,
     url: "https://calyaan.netlify.app/",
   },
-  {
-    name: "Rm Mandados",
-    logo: logoRm,
-    url: "https://kaldi-decap.netlify.app/",
-  },
-  {
-    name: "Yerba Mate Alborada",
-    logo: logoalborada,
-    url: "https://yerbamatealborada.com/",
-  },
+  
   {
     name: "CryptoSeed.ai",
     logo: logocrypto,
@@ -54,6 +41,22 @@ const partnerLogos = [
     url: "https://globalhomegroup.com.ar/",
   },
   {
+    name: "Rm Mandados",
+    logo: logoRm,
+    url: "https://kaldi-decap.netlify.app/",
+  },
+ 
+  {
+    name: "Store Computer",
+    logo: "https://static.vecteezy.com/system/resources/thumbnails/009/096/946/small/monitor-screen-computer-with-colorful-rainbow-bubble-illustration-logo-design-vector.jpg", // Reemplazar con la URL real
+    url: "https://storecomputer.netlify.app/",
+  },
+  {
+    name: "El Gato Negro Premium - Eliquids",
+    logo: logo1,
+    url: "https://elgatonegropremium.netlify.app/",
+  },
+  {
     name: "Moda Total",
     logo: logomodatotal,
     url: "https://modatotal.netlify.app/",
@@ -62,6 +65,11 @@ const partnerLogos = [
     name: "Lua Collection Circuito",
     logo: logolua,
     url: "https://luacollection.netlify.app/",
+  },
+  {
+    name: "July Marroquinería",
+    logo: logojuly,
+    url: "https://july-marroquineria.netlify.app/",
   },
 ];
 
@@ -99,8 +107,8 @@ export const PartnerLogos = () => {
           viewport={{ once: true }}
           transition={{ duration: 0.5, delay: 0.2 }}
         >
-          <h3>19</h3>
-          <p>Projects Completed</p>
+          <h3>Más de 20</h3>
+          <p>Projectos Completados</p>
         </motion.div>
         <motion.div
           className="stat-item"
@@ -109,8 +117,8 @@ export const PartnerLogos = () => {
           viewport={{ once: true }}
           transition={{ duration: 0.5, delay: 0.4 }}
         >
-          <h3>5</h3>
-          <p>Countries</p>
+          <h3>Clientes en 6 países</h3>
+          <p>Argentina, Paraguay, Colombia, Estados Unidos, Australia, Brasil</p>
         </motion.div>
         <motion.div
           className="stat-item"
@@ -119,8 +127,8 @@ export const PartnerLogos = () => {
           viewport={{ once: true }}
           transition={{ duration: 0.5, delay: 0.6 }}
         >
-          <h3>5</h3>
-          <p>Years of Experience</p>
+          <h3>Mas de 5</h3>
+          <p>Años de experiencia en desarrollo web</p>
         </motion.div>
       </div>
     </div>
